2a8c01a9d4234905d61f6d651b915a73230e2949
Santiago Rodriguez Murialdo <santiromur@gmail.com>
2023-02-16 10:32:28 -0600

    JS 3

---
+++ Laboratorio 4/ex3.js
@@ -0,0 +1,39 @@
+var arr3 = []
+var size = parseInt(window.prompt('Tamaño del arreglo'))
+
+for(var a=0; a<size; a++) 
+{  
+    arr3[a] = prompt('Enter array Element ' + (a+1));
+}
+
+const neg = []
+const igual = []
+const mayor = []
+
+function contador(arr) {
+    for (const element of arr){
+        if (element < 0){
+            neg.push(element)
+        }
+        else if(element == 0){
+            igual.push(element)
+        }
+        else{
+            mayor.push(element)
+        }
+    }
+    var strF = 
+    `
+    Cantidad de números negativos: ${neg.length()}
+    Cantidad de ceros: ${igual.length()}
+    Cantidad de números mayores a 0: ${mayor.length()} 
+    `
+
+    return strF;
+}
+
+document.write(
+    `
+    <p>${contador(arr3)}</p>
+    `
+)
