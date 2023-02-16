6bc27cc8a9bc53421d9148413425d88c12e48975
Santiago Rodriguez Murialdo <santiromur@gmail.com>
2023-02-16 10:00:47 -0600

    JS 1

---
+++ Laboratorio 4/ex1.js
@@ -0,0 +1,33 @@
+const num1 = parseInt(window.prompt("Ingresa un número"));
+const arr1 = [];
+
+for (let index = 1; index < num1 + 1; index++) {
+  arr1.push(index);
+}
+
+const strArr = []
+for (let i=0; i < arr1.length; i++){
+    let string = 
+    `<tr>
+        <td>${arr1[i]}</td>
+        <td>${arr1[i] ** 2}</td>
+        <td>${arr1[i] ** 3}</td>
+    </tr>`
+    strArr.push(string)
+}
+
+const table1 = document.write(
+  `<table class="tg">
+    <caption>Cuadrados y Cubos</caption>
+    <thead>
+        <tr>
+            <th>Numero</th>
+            <th>Cuadrado</th>
+            <th>Cubo</th>
+        </tr>
+    </thead>
+    <tbody>
+        ${strArr.toString()}
+    </tbody>
+   </table>`
+);
